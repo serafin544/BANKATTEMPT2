c2a9553404d6b6569b677aa735a8c508352a7882
serafin <serafinthedream@outlook.com>
2019-11-20 09:28:10 -0500

    fixedH2

--- src/main/java/com/example/MacFin/MacFinApplication.java
+++ src/main/java/com/example/MacFin/MacFinApplication.java
@@ -4,7 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MacFinApplication {
+public class
+MacFinApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MacFinApplication.class, args);
